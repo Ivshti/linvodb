6a2f4f4f7b693ea283b6da0e724f783561444a1d
Ivo Georgiev <ivo@linvo.org>
2014-01-12 19:28:49 +0200

    Create a DB directory for the app

--- linvodb.js
+++ linvodb.js
@@ -1,9 +1,15 @@
 var nedb = require("nedb");
+var mkdirp = require("mkdirp");
+var path = require("path");
 
-function LinvoDB()
+function LinvoDB(dataPath)
 {
-    var linvodb = {};
+    var linvodb = {},
+        dbPath = path.join(dataPath+"", "db"); // since the object can be from node-webkit, it might be detected as..not a string; so concat it with an empty one
+
+    mkdirp.sync(dbPath);
     
+
     return linvodb;
 };
 
