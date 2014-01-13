443155123a4ebb0ce0ef163605dda6d8dee0bef9
Ivo Georgiev <ivo@linvo.org>
2014-01-14 00:50:50 +0200

    initialize the store

--- linvodb.js
+++ linvodb.js
@@ -4,16 +4,21 @@ var path = require("path");
 
 function LinvoDB(dataPath)
 {
-    var linvodb = {},
+    var linvodb = { },
         dbPath = path.join(dataPath+"", "db"); // since the object can be from node-webkit, it might be detected as..not a string; so concat it with an empty one
 
     mkdirp.sync(dbPath);
     
     /* The model constructor - this creates a model
      */
+    linvodb.models = {}; // An easy way to access all models
     linvodb.Model = function Model(name, schema, options)
     {
         options = options || {};
+        
+        var model = linvodb.models[name] = { };
+        var db = model.store = new nedb({ filename: path.join(dbPath, name), autoload: true });// expose that
+        
         // TODO: err check, validation
     };
     
