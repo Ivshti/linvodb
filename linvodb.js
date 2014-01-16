11d5bc7e44cd8fd8c7e52642e62aa7f77849f8ae
Ivo Georgiev <ivo@linvo.org>
2014-01-16 17:27:34 +0200

    LinvoDB is now a singleton - you initialize it once with .init(path)

--- linvodb.js
+++ linvodb.js
@@ -4,121 +4,123 @@ var path = require("path");
 var _ = require("underscore");
 var EventEmitter = require("events").EventEmitter;
 
-function LinvoDB(dataPath)
+
+var linvodb = { };
+
+linvodb.init = function(dataPath)
 {
-    var linvodb = { },
-        dbPath = path.join(dataPath+"", "db"); // since the object can be from node-webkit, it might be detected as..not a string; so concat it with an empty one
-    mkdirp.sync(dbPath);
-    
-    /* The model constructor - this creates a model
-     */
-    linvodb.models = {}; // An easy way to access all models
-    linvodb.Model = function Model(name, schema, options)
-    {
-        if (typeof(name) != "string") throw new Error("model name must be a string");
-        if (typeof(schema) != "object") throw new Error("model schema must be an object");
-        options = options || {};
-        
-        var db = new nedb({ filename: path.join(dbPath, name), autoload: true });
-
-        /* Inflate the schema object / create the base document
-         */
-        var fullSchema = {};
-        _.each(schema, function(val, key)
-        {
-            if (typeof(val) == "object" && val.type) fullSchema[key] = val;
-            fullSchema[key] = { type: val };
-        }); 
-        
-        var baseDoc = {};
-        _.each(fullSchema, function(val, key)
-        {
-            baseDoc[key] = val.default || new val.type();
-        });
-        
-        
-        /* Small helpers/utilities
-         */
-        var hookEvent = function(ev, fn) {
-            return function() {
-                model.emit(ev);
-                fn.apply(this, arguments);
-            };
-        };
-
-        /* The instance constructor
-         */
-        var model = linvodb.models[name] = function Document(doc) 
-        {
-            if (doc && doc.constructor.name == "Document")
-                return doc;
-
-            _.extend(this, baseDoc, doc || {});
-            this.validate();
-        };
-        var toModelInstance = function(x) { return new model(x) };
-        
-        /* Instance methods
-         */
-        model.prototype.validate = function()
-        {
-            // TODO
-        };
-        model.prototype.save = function(cb)
-        {
-            this.validate();
-            var doc = this,
-                callback = function(err) { cb(err, doc) };
-            
-            db.findOne({ _id: doc._id }, function(err, isIn)
-            {
-                if (isIn) db.update({ _id: doc._id }, { $set: doc }, {}, hookEvent("updated", callback));
-                else db.insert(doc, hookEvent("updated", callback));
-            });
-        };
-        model.prototype.remove = function(cb) { db.remove({ _id: this._id }, hookEvent("updated", cb)) };
-        
-        
-        /* Static methods
-         */
-        //model.virtual
-        //model.static
-        //model.method
-        
-        // Query
-        model.find = function(query, cb) 
-        {
-            db.find(query, function(err, res)
-            {
-                cb(err, res && res.map(toModelInstance));
-            });
-        };
-        model.count = function(query, cb) { db.count(query, cb) };
-
-        // Modification
-        model.remove = function(query, options, cb) { db.remove(query, options, hookEvent("updated", cb)) };
-        model.update = function(query, update, options, cb) { db.update(query, update, options, hookEvent("updated", cb)) };
-        model.insert = function(docs, cb) { db.insert(docs.map(toModelInstance), hookEvent("updated", cb)) };
-
-        // Support event emitting
-        _.extend(model, new EventEmitter());
-
-        model.modelName = name;
-        model.store = db;
-        return model;
-    };
-    
-    
-    linvodb.createService = function(module, model)
-    {
-        module.factory(model.modelName, ["$rootScope", function($rootScope) 
-        {
-            model.on("update", function() { $rootScope.$apply() });
-            return model;
-        }]);
-    };
-
-    return linvodb;
+    linvodb.dbPath = path.join(dataPath+"", "db"); // since the object can be from node-webkit, it might be detected as..not a string; so concat it with an empty one
+    mkdirp.sync(linvodb.dbPath);
 };
 
-module.exports = LinvoDB;
+
+/* The model constructor - this creates a model
+ */
+linvodb.models = {}; // An easy way to access all models
+linvodb.Model = function Model(name, schema, options)
+{
+    if (! linvodb.dbPath) throw new Error("must initialize the DB first - .init(dbPath)");
+    if (typeof(name) != "string") throw new Error("model name must be a string");
+    if (typeof(schema) != "object") throw new Error("model schema must be an object");
+    options = options || {};
+    
+    var db = new nedb({ filename: path.join(linvodb.dbPath, name), autoload: true });
+
+    /* Inflate the schema object / create the base document
+     */
+    var fullSchema = {};
+    _.each(schema, function(val, key)
+    {
+        if (typeof(val) == "object" && val.type) fullSchema[key] = val;
+        fullSchema[key] = { type: val };
+    }); 
+    
+    var baseDoc = {};
+    _.each(fullSchema, function(val, key)
+    {
+        baseDoc[key] = val.default || new val.type();
+    });
+    
+    
+    /* Small helpers/utilities
+     */
+    var hookEvent = function(ev, fn) {
+        return function() {
+            model.emit(ev);
+            fn.apply(this, arguments);
+        };
+    };
+
+    /* The instance constructor
+     */
+    var model = linvodb.models[name] = function Document(doc) 
+    {
+        if (doc && doc.constructor.name == "Document")
+            return doc;
+
+        _.extend(this, baseDoc, doc || {});
+        this.validate();
+    };
+    var toModelInstance = function(x) { return new model(x) };
+    
+    /* Instance methods
+     */
+    model.prototype.validate = function()
+    {
+        // TODO
+    };
+    model.prototype.save = function(cb)
+    {
+        this.validate();
+        var doc = this,
+            callback = function(err) { cb(err, doc) };
+        
+        db.findOne({ _id: doc._id }, function(err, isIn)
+        {
+            if (isIn) db.update({ _id: doc._id }, { $set: doc }, {}, hookEvent("updated", callback));
+            else db.insert(doc, hookEvent("updated", callback));
+        });
+    };
+    model.prototype.remove = function(cb) { db.remove({ _id: this._id }, hookEvent("updated", cb)) };
+    
+    
+    /* Static methods
+     */
+    //model.virtual
+    //model.static
+    //model.method
+    
+    // Query
+    model.find = function(query, cb) 
+    {
+        db.find(query, function(err, res)
+        {
+            cb(err, res && res.map(toModelInstance));
+        });
+    };
+    model.count = function(query, cb) { db.count(query, cb) };
+
+    // Modification
+    model.remove = function(query, options, cb) { db.remove(query, options, hookEvent("updated", cb)) };
+    model.update = function(query, update, options, cb) { db.update(query, update, options, hookEvent("updated", cb)) };
+    model.insert = function(docs, cb) { db.insert(docs.map(toModelInstance), hookEvent("updated", cb)) };
+
+    // Support event emitting
+    _.extend(model, new EventEmitter());
+
+    model.modelName = name;
+    model.store = db;
+    return model;
+};
+
+
+linvodb.createService = function(module, model)
+{
+    module.factory(model.modelName, ["$rootScope", function($rootScope) 
+    {
+        model.on("update", function() { $rootScope.$apply() });
+        return model;
+    }]);
+};
+
+module.exports = linvodb;
