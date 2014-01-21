59d4db5a927fb12f2fa773ee5c57b43101b720e6
Ivo Georgiev <ivo@linvo.org>
2014-01-21 02:37:12 +0200

    Basic sync code; does not work for now, just copy-paste-adapt from old code

---
+++ sync.js
@@ -0,0 +1,84 @@
+var async = require("async"),
+    _ = require("underscore");
+
+module.exports = function setupSync(model, collection, api)
+{
+    var dirty = false;
+    model.on("update", function()
+    { 
+        dirty = true;
+        q.push({}, cb);
+    }); // TODO: somehow export a method to force-trigger the sync; model.static('triggerSync') ?
+
+    /* We need to run only one task at a time */
+    var q = async.queue(function(opts, cb)
+    {
+        if (! api.user) return cb();
+        
+        var syncInfo = /*db.findOne("_sync", { collection: collection }) ||*/ { collection: model.modelName, lastSync: 0 },
+            remoteMeta, localMeta, modifications, deletes,
+            baseQuery = { collection: remoteCollection || model.modelName };
+
+        async.auto({
+            retrieve_remote: function(callback)
+            {
+                api.request("datastoreMeta", { collection: remoteCollection || model.modelName }, function(err, meta)
+                {
+                    remoteMeta = meta;
+                    callback(err);
+                });
+            },
+            retrieve_local: function(callback)
+            {
+                collection.find({}, function(err, results)
+                {
+                    localMeta = results.map(function(r) { return [r._id, r._mtime, 1] });
+                    callback(err);
+                });
+            },
+            compile_changes: ["retrieve_remote", "retrieve_local", function(callback)
+            {
+                // It's correct to mark the DB before commiting the changes, but when compiling the list of changes
+                // Until the changes are commited, more changes might occur
+                dirty = false;
+                
+                modifications = [].concat(remoteMeta).concat(localMeta)
+                .filter(function(m) { return (m[1] || 0) >= syncInfo.lastSync })
+                .sort(function(a, b) { return a[1] - b[1] });
+                /* TODO: solve conflicts
+                 */
+                
+                /* Slightly tricky part: deletes; we just need all IDs that are not here, but on the remote server, 
+                 * which were not modified between lastSync and now */
+                deletes = _.difference(
+                    _.difference(remoteMeta.map(function(m){ return m[0] }), localMeta.map(function(m){ return m[0] })),
+                    modifications.map(function(m){ return m[0] })
+                );
+                
+                callback();
+            }],
+            push_remote: ["compile_changes", function(callback)
+            {
+                var ids = modifications.filter(function(m) { return m[2] }).map(function(m) { return m[0] });
+                collection.find({ _id: { $in: ids } }, function(err, updatedItems)
+                {
+                    api.request("datastorePut", _.extend({ }, baseQuery, { changes: 
+                        deletes.map(function(id) { return { _id: id, _delete: true } })
+                        .concat(updatedItems)
+                    }), callback);
+                });
+            }],
+            push_local: ["compile_changes", function(callback)
+            {
+                api.request("datastoreGet", _.extend({ }, baseQuery, { 
+                    ids: modifications.filter(function(m) { return ! m[2] }).map(function(m) { return m[0] })
+                }), function(err, results) { collection.insert(results, callback) }); // TODO: some of those might be updates?
+            }],
+            update_last_sync: ["push_remote", "push_local", function(callback)
+            {
+                syncInfo.lastSync = Date.now();
+                //db.save("_sync", syncInfo, callback)
+            }]
+        }, cb);
+    }, 1);
+}
